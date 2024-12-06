f70c057d57d827ff09831f139aa2166c137addd9
Ali Alahdal <aloshegamer@gmail.com>
2024-12-06 21:09:33 +0300

    Update settings.py

--- Django_Messaging_And_Calling/settings.py
+++ Django_Messaging_And_Calling/settings.py
@@ -104,19 +104,20 @@ CHANNEL_LAYERS = {
     },
 }
 
-
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        'NAME': 'dbtest',
-        'USER': 'postgres',
-        'PASSWORD': 'Aa0559264432.',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     'NAME': 'dbtest',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'Aa0559264432.',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432'
+    # }
 }
 
 
