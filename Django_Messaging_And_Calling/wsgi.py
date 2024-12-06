a2d3be0115d977e6200a58638015a93dd4ccbabf
Ali Alahdal <aloshegamer@gmail.com>
2024-12-06 21:11:55 +0300

    Update wsgi.py

--- Django_Messaging_And_Calling/wsgi.py
+++ Django_Messaging_And_Calling/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django_Messaging_And_Calling.settings")
 
+
+
 application = get_wsgi_application()
+app = application
