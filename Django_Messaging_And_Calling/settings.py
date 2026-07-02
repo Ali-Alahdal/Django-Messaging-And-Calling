3b45aa3ede91f3f4ed1218db8ae847604caff206
Ali Alahdal <aloshegamer@gmail.com>
2026-07-03 00:52:43 +0300

    Remove secret key from settings.py
    Removed the secret key from settings for security reasons.

--- Django_Messaging_And_Calling/settings.py
+++ Django_Messaging_And_Calling/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-42u6n7fq5o4v2t2%tyq@@u5ehaoz)jps!0wr90op97k)ucl3)="
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
